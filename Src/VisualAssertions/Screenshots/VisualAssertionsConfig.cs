78797aea6d9eb01457bcbf1b34b09ac08075a923
rafal.palej <rafal.palej@ailleron.com>
2021-04-23 13:01:03 +0200

    rebase fix

--- Src/VisualAssertions/Screenshots/VisualAssertionsConfig.cs
+++ Src/VisualAssertions/Screenshots/VisualAssertionsConfig.cs
@@ -15,34 +15,4 @@ namespace Tellurium.VisualAssertions.Screenshots
         /// </summary>
         public IScreenshotComparisonStrategy ScreenshotComparisonStrategy { get; set; }
     }
-
-    public class PixelByPixelComparisonParameters
-    {
-        /// <summary>
-        /// Percentage of pixels that can be different when matching two images.
-        /// If percentage of pixels that are different is greater than this value then the images are considered unmatched
-        /// </summary>
-        /// <remarks>
-        /// Accepted values: <0.0, 100.0>
-        /// </remarks>
-        public double MaxPercentOfUnmatchedPixels { get; }
-
-        /// <summary>
-        /// Number of pixels that can be different when matching two images.
-        /// If number of pixels that are different is greater than this value then the images are considered unmatched
-        /// </summary>
-        public uint PixelToleranceCount { get; }
-
-        public PixelByPixelComparisonParameters(double maxPercentOfUnmatchedPixels)
-        {
-            MaxPercentOfUnmatchedPixels = maxPercentOfUnmatchedPixels;
-            PixelToleranceCount = uint.MaxValue;
-        }
-
-        public PixelByPixelComparisonParameters(uint pixelToleranceCount)
-        {
-            PixelToleranceCount = pixelToleranceCount;
-            MaxPercentOfUnmatchedPixels = 100.0;
-        }
-    }
 }
